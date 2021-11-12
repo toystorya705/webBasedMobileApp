cc2d211b6cb27f3ca6290261a18bbdb954236380
Aryan Gupta <toystorya705@gmail.com>
2021-11-12 04:16:19 +0000

    Files added

---
+++ index.js
@@ -0,0 +1,74 @@
+
+let app = new Vue({
+    el: "#app",
+    data: {
+
+        product: product,
+        showProduct: true,
+        order: {
+            firstName: "",
+            lastName: "",
+            address: "",
+            city: "",
+            zip: "",
+            method: 'Home',
+            gift: "Do not send as a gift",
+            sendGift: 'Send as a gift',
+            dontSendGift: 'Do not send as a gift',
+            states: [
+                'Alabama',
+                'Arizona',
+                'California',
+                'Nevada'
+            ],
+            stateCheckoutDisplay: ""
+
+
+        },
+
+        cart: []
+    },
+    methods: {
+        //  This function decrease the value of stock by 1 every time user click Button
+
+        addItem(itemId) {
+            if (product[itemId].stock > 0) {
+
+                this.stock = --product[itemId].stock;
+                this.cart.push(itemId);
+            }
+        },
+        isDisabled(itemId) {
+            return this.product[itemId].stock === 0
+        },
+        submitForm() { alert('Ordersubmitted!') },
+        stateSelect(state) {
+            //     console.log(state);
+            //     this.order.stateCheckoutDisplay= state;
+
+        },
+        showCheckout() {
+            this.showProduct = this.showProduct ? false : true
+            
+        },
+   
+        cartCount(id) {
+            let count = 0;
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] === id) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    },
+    computed: {// This disables the button at 0 stock
+        cartCheckDisable: function(){
+           return this.cart.length===0;
+        },
+        cartItemCount: function () {
+            return this.cart.length || '';
+        }
+
+    }
+});
---
+++ products.js
@@ -0,0 +1,98 @@
+let stock=7;
+let product=[{
+
+        subject: "English Litreature",
+        location: "london",
+        price: "50",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:3,
+        id:0
+    },{
+
+        subject: "Math",
+        location: "l0",
+        price: "100",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:4,
+        id:1
+    },{
+
+        subject: "English Language",
+        location: "london",
+        price: "90",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:2,
+        id:2
+    },
+    {
+
+        subject: "Science",
+        location: "london",
+        price: "120",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:5,
+        id:3
+    },{
+
+        subject: "EVS",
+        location: "london",
+        price: "9",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:1,
+        id:4
+    },{
+
+        subject: "Hindi",
+        location: "london",
+        price: "30",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:5,
+        id:5
+    },
+    {
+
+        subject: "French",
+        location: "london",
+        price: "49",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:3,
+        id:6
+    },
+    {
+
+        subject: "Sanskrit",
+        location: "london",
+        price: "500",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:2,
+        id:7
+    },
+    {
+
+        subject: "German",
+        location: "london",
+        price: "49",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:3,
+        id:8
+    },
+    {
+
+        subject: "Chinese",
+        location: "london",
+        price: "300",
+        stock: stock,
+        image: "https://media4.giphy.com/media/9d1Fo0XyIYXzW/200.gif",
+        rating:2,
+        id:9
+    }
+]
